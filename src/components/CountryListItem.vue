5220486efb0095613f7c06a5fd514c70cbbc0782
droidsam1 <droidsam@gmail.com>
2022-08-10 16:41:47 +0200

    load country list from file. add component CountryListItem

---
+++ src/components/CountryListItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <a class="list-group-item list-group-item-action" :href="getLinkToDetail">
+    <img :src="getFlag" />
+    <p>{{ name }}</p>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    alpha2Code: String,
+    alpha3Code: String,
+  },
+
+  computed: {
+    getFlag() {
+      return `https://flagpedia.net/data/flags/icon/72x54/${this.alpha2Code?.toLowerCase()}.png`;
+    },
+    getLinkToDetail() {
+      return "/" + this.alpha3Code;
+    },
+  },
+};
+</script>
+
+<style></style>
