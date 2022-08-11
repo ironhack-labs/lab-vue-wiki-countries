c0503c4dd8686a5455813a0d152f1e29beb3dc63
droidsam1 <droidsam@gmail.com>
2022-08-11 18:43:02 +0200

    separate countrylist view from component

---
+++ src/components/CountriesList.vue
@@ -0,0 +1,31 @@
+<template>
+  <CountryListItem
+    v-for="country in countries"
+    v-bind:key="country.alpha3Code"
+    :name="country?.name?.common"
+    :alpha2Code="country.alpha2Code"
+    :alpha3Code="country.alpha3Code"
+  />
+</template>
+
+<script>
+import CountryListItem from "./CountryListItem.vue";
+import countriesJson from "../../public/countries.json";
+export default {
+  data() {
+    return {
+      countries: [],
+    };
+  },
+  props: {
+    countryList: Array,
+  },
+  components: { CountryListItem },
+
+  mounted() {
+    this.countries = countriesJson;
+  },
+};
+</script>
+
+<style></style>
--- src/pages/CountriesListView.vue
+++ src/pages/CountriesListView.vue
@@ -2,33 +2,16 @@
   <!-- Countries List (Bootstrap column) -->
   <div class="col-5" style="max-height: 90vh; overflow: scroll">
     <div class="list-group text-center">
-      <CountryListItem
-        v-for="country in countries"
-        v-bind:key="country.alpha3Code"
-        :name="country?.name?.common"
-        :alpha2Code="country.alpha2Code"
-        :alpha3Code="country.alpha3Code"
-      />
+      <CountryList />
     </div>
   </div>
 </template>
 
 <script>
-import CountryListItem from "../components/CountryListItem.vue";
-import countriesJson from "../../public/countries.json";
+import CountryList from "../components/CountriesList.vue";
 export default {
-  data() {
-    return {
-      countries: [],
-    };
-  },
-  props: {
-    countryList: Array,
-  },
-  components: { CountryListItem },
-
-  mounted() {
-    this.countries = countriesJson;
+  components: {
+    CountryList,
   },
 };
 </script>
