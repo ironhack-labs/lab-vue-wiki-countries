9a1b662dcc8d13e975cb7f8f33bd948e4b1c5791
droidsam1 <droidsam@gmail.com>
2022-08-13 08:45:20 +0200

    add spinner component

---
+++ src/components/TextWithSpinner.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="loading">
+    {{ text }}
+    <Spinner />
+  </div>
+</template>
+
+<script>
+import Spinner from "./Spinner.vue";
+export default {
+  components: { Spinner },
+  props: {
+    text: String,
+  },
+};
+</script>
+
+<style scoped>
+.loading {
+  display: flex;
+  flex-flow: row;
+}
+</style>
